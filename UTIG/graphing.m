9e33c80ccbce7ce7fe1597047c27399164669c7f
Conor Andersen <conordandersen@gmail.com>
2023-08-02 12:48:05 -0500

    added plot of gps vert_cor vs radar

--- UTIG/graphing.m
+++ UTIG/graphing.m
@@ -3,22 +3,22 @@ datapath = 'MAG01b';
 
 % call to graphing function for laser data.
 % edit variable names (laserfig1,magfig1,gpsfig1...) as needed.
+% change file path and name (laserfig1) as desired for all print functions.
 laserfig1 = graphlaser(datapath);
 set(gcf,'position',[400,100,1000,1000]);
-% change file path and name (laserfig1) as desired for all print functions.
-print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/laserfig1','-dpng');
+%print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/MAG01a_laserfig','-dpng');
 
 % call to graphmag function.
 magfig1 = graphmag(datapath);
 set(gcf,'position',[400,100,1000,1000]);
-print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/magfig1','-dpng');
+print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/MAG01b_magfig','-dpng');
 
 %call to graphgps function.
 gpsfig1 = graphgps(datapath);
 set(gcf,'position',[400,100,1000,1000]);
-print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/gpsfig1','-dpng');
+%print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/MAG01a_gpsfig','-dpng');
 
 %call to radar graphing function.
 radarfig1= graphradar(datapath);
 set(gcf,'position',[400,100,1000,1000]);
-print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/radarfig1','-dpng');
+%print('/Users/conorandersen/Desktop/UTIG_Project/UTIG/Figures/MAG01a_radarfig','-dpng');
--- UTIG/graphradar.m
+++ UTIG/graphradar.m
@@ -6,6 +6,8 @@ bf1path = strcat(datapath,'/BF1_UBHa/vmaxpos_tim');
 bf1path2 = strcat(datapath,'/BF1_UBHa/syn_itim');
 bf2path = strcat(datapath,'/BF2_UBHa/vmaxpos_tim');
 bf2path2 = strcat(datapath,'/BF2_UBHa/syn_itim');
+gpspath1 = strcat(datapath,'/GPS_UBHa/syn_itim');
+gpspath2 = strcat(datapath,'/GPS_UBHa/vert_cor');
 
 bf1_vmaxpos_tim = load(bf1path);
 bf2_vmaxpos_tim = load(bf2path);
@@ -13,19 +15,24 @@ syn_itim_bf1 = load(bf1path2);
 syn_itim_bf2 = load(bf2path2);
 load(laspath1);
 syn_itim_las = load(laspath2);
+syn_itim_gps = load(gpspath1);
+load(gpspath2);
 
 time_bf1 = (syn_itim_bf1/10000)/60;
 time_bf2 = (syn_itim_bf2/10000)/60;
-bf1_two_way_travel_time = (bf1_vmaxpos_tim).*(1*10^6);
-bf2_two_way_travel_time = (bf2_vmaxpos_tim).*(1*10^6);
-bf1_travel_time = ((bf1_vmaxpos_tim).*(1*10^6))/2;
-bf2_travel_time = ((bf2_vmaxpos_tim).*(1*10^6))/2;
+bf1_two_way_travel_time = bf1_vmaxpos_tim.* 1e6;
+bf2_two_way_travel_time = bf2_vmaxpos_tim.* 1e6;
+bf1_travel_time = (bf1_vmaxpos_tim.* 1e6)/2;
+bf2_travel_time = (bf2_vmaxpos_tim.* 1e6)/2;
 xlas = (syn_itim_las/10000)/60;
 ylas = las_rng;
 
-las_travel_time = ((las_rng)/(3*10^8))*(1*10^6);
-bf1_radar_distance = (bf1_vmaxpos_tim)*(3*10^8);
-bf2_radar_distance = (bf2_vmaxpos_tim)*(3*10^8);
+las_travel_time = ((las_rng)/(3e8))* 1e6;
+bf1_radar_distance = bf1_vmaxpos_tim * 3e8;
+bf2_radar_distance = bf2_vmaxpos_tim * 3e8;
+
+xgps = (syn_itim_gps/10000)/60;
+ygps = vert_cor;
 
 radarfig = figure
        
@@ -43,19 +50,6 @@ radarfig = figure
 
        subplot(3,1,2)
        hold on
-       plot(xlas,las_travel_time)
-       plot(time_bf1,bf1_two_way_travel_time)
-       plot(time_bf2,bf2_two_way_travel_time)
-       ax2 = gca;
-       set(ax2,'YDir','reverse');
-       xlabel('Time(minutes)')
-       ylabel('Travel Time(micro seconds)')
-       title('Radar Travel Time vs Laser Travel Two Way Time')
-       legend('Laser Time','BF1 Radar Time','BF2 Radar Time','location','northeastoutside')
-       hold off
-
-       subplot(3,1,3)
-       hold on
        plot(xlas,ylas)
        plot(time_bf1,bf1_radar_distance)
        plot(time_bf2,bf2_radar_distance)
@@ -67,6 +61,16 @@ radarfig = figure
        legend('Laser Distnce','BF1 Radar Distance','BF2 Radar Distance','location','northeastoutside')
        hold off
 
+       subplot(3,1,3)
+       hold on
+       plot(xgps,ygps)
+       plot(time_bf1,bf1_radar_distance)
+       plot(time_bf2,bf2_radar_distance)
+       xlabel('Time (minutes)')
+       ylabel('Distance (meters)')
+       title('GPS elevation vs radar distance')
+       legend('GPS','BF1','BF2')
+       hold off
 
 end
 
